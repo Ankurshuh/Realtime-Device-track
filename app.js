c3f0d0076a216b0101645afd7315ff7d99a4fed0
134132673+Ankurshuh@users.noreply.github.com <user.email.link"shuklaankur457@gmail.com>
2026-01-03 14:56:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express= require("express");
+const app =express();
+const http = require("http");
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+const path =require("path");
+
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+io.on("connecton",function(socket){
+    console.log("new user connected");
+ });
+app.get("/",function(req,res){
+    res.render("index");
+}); 
+
+server.listen(8000);
